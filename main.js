c7b1bbc6a259cb3f12e974c6073b030eac243806
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2017-02-22 10:02:00 -0700

    object assign exercise finished

---
+++ main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/*
+
+Task #1:
+- Using Object.assign(), create an object "moreLanguages" that combines the following key/value pairs with the existing object "languages":
+
+  {
+    isolates: ["Korean", "Basque"],
+    Germanic: ["Swedish", "Danish", "English"]
+  }
+
+Logging "moreLanguages" should print:
+
+  {
+    Celtic: [ 'Welsh', 'Irish', 'Breton' ],
+    AfroAsiatic: [ 'Hebrew', 'Somali', 'Oromo' ],
+    isolates: [ 'Korean', 'Basque' ],
+    Germanic: [ 'Swedish', 'Danish', 'English' ]
+  }
+
+*/
+
+let languages = {
+  Celtic: ["Welsh", "Irish", "Breton"],
+  AfroAsiatic: ["Hebrew", "Somali", "Oromo"]
+};
+
+//Write task #1 code below
+
+
+
+
+/*
+
+
+*/
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Creating Objects in Javascript
+
+- Copy objects with ```Object.assign```
+- Create objects with explicit prototypes with ```Object.create```
